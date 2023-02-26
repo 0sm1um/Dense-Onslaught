e374e19b90c0d4c3e67b6c149b841b4275fd5976
John Hiles <hilesj@vcu.edu>
2023-02-26 12:46:56 -0500

    Tweaked ambient spawning

--- scripts/mods/Dense Onslaught/Mutator/ambient.lua
+++ scripts/mods/Dense Onslaught/Mutator/ambient.lua
@@ -10,28 +10,39 @@ local mod = get_mod("Dense Onslaught")
 	UnitVariationSettings.skaven_storm_vermin.material_variations.cloth_tint.max = 31
 	UnitVariationSettings.skaven_storm_vermin.material_variations.skin_tint.min = 1
 	UnitVariationSettings.skaven_storm_vermin.material_variations.skin_tint.max = 1
-	BuffTemplates.healing_standard.buffs = {
-		{
-			multiplier = 0.25,
-			stat_buff = "damage_dealt",
-			name = "curse_khorne_champions_damage_buff",
-			max_stacks = 1
-		}
-	}
 	BeastmenStandardTemplates.healing_standard.radius = 10
 	UtilityConsiderations.beastmen_place_standard.distance_to_target.max_value = 15
-	RecycleSettings.max_grunts = 165
-	RecycleSettings.push_horde_if_num_alive_grunts_above = 300
-	PackSpawningSettings.default.area_density_coefficient = 0.15
-	PackSpawningSettings.default_light.area_density_coefficient = 0.15
-	PackSpawningSettings.skaven.area_density_coefficient = 0.15
-	PackSpawningSettings.skaven_light.area_density_coefficient = 0.15
-	PackSpawningSettings.chaos.area_density_coefficient = 0.15
-	PackSpawningSettings.chaos_light.area_density_coefficient = 0.15
-	PackSpawningSettings.beastmen.area_density_coefficient = 0.15
-	PackSpawningSettings.beastmen_light.area_density_coefficient = 0.15
-	PackSpawningSettings.skaven_beastmen.area_density_coefficient = 0.15
-	PackSpawningSettings.chaos_beastmen.area_density_coefficient = 0.15
+	-- This code makes beastmen banners increase enemy's damage.
+	
+	--BuffTemplates.healing_standard.buffs = {
+	--	{
+	--		multiplier = 0.25,
+	--		stat_buff = "damage_dealt",
+	--		name = "curse_khorne_champions_damage_buff",
+	--		max_stacks = 1
+	--	}
+	--}
+	
+	--[[
+	My rationale for these settings are as follows:
+	I want increased ambients compared to base onslaught to slow down progression through map.
+	However, I want more skaven ambients due to their lower HP.
+	Increasing max_grunts means players can be caught out of position more if they can't get to
+	their hold spot in time.
+	--]]
+	
+	RecycleSettings.max_grunts = 165                                       -- More than Ons+, Less than Dutch
+	RecycleSettings.push_horde_if_num_alive_grunts_above = 250             -- More than Ons+, Less than Dutch
+	PackSpawningSettings.default.area_density_coefficient = 0.12           -- Same as Ons+
+	PackSpawningSettings.default_light.area_density_coefficient = 0.12     -- Same as Ons+
+	PackSpawningSettings.skaven.area_density_coefficient = 0.15	           -- Same as Dutch
+	PackSpawningSettings.skaven_light.area_density_coefficient = 0.15      -- Same as Dutch
+	PackSpawningSettings.chaos.area_density_coefficient = 0.13			   -- More than Ons+, Less than Dutch
+	PackSpawningSettings.chaos_light.area_density_coefficient = 0.13	   -- More than Ons+, Less than Dutch
+	PackSpawningSettings.beastmen.area_density_coefficient = 0.13          -- More than Ons+, Less than Dutch
+	PackSpawningSettings.beastmen_light.area_density_coefficient = 0.13    -- More than Ons+, Less than Dutch
+	PackSpawningSettings.skaven_beastmen.area_density_coefficient = 0.14   -- More than Ons+, Less than Dutch
+	PackSpawningSettings.chaos_beastmen.area_density_coefficient = 0.13    -- More than Ons+, Less than Dutch
 
 	PackSpawningSettings.default.difficulty_overrides = nil
 	PackSpawningSettings.skaven.difficulty_overrides = nil
