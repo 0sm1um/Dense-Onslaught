0ef13a384f7cd079d96d48095397e3f12288e756
dalokraff <87997987+dalokraff@users.noreply.github.com>
2023-05-25 02:30:22 -0500

    Update director_utils.lua

--- scripts/mods/Dense Onslaught/directors/director_utils.lua
+++ scripts/mods/Dense Onslaught/directors/director_utils.lua
@@ -22,12 +22,12 @@ DirectorUtils.add_new_ConflictDirector = function (name)
         debug_color = "green",
         disabled = false,
         intensity = DirectorUtils.add_to_settings(name, IntensitySettings, mod:dofile("scripts/mods/Dense Onslaught/directors/"..name.."/intensity_settings")),
-        pacing = DirectorUtils.add_to_settings(name, PacingSettings, mod:dofile("scripts/mods/Dense Onslaught/directors/"..name.."/pacing_settings")),
         boss = DirectorUtils.add_to_settings(name, BossSettings, mod:dofile("scripts/mods/Dense Onslaught/directors/"..name.."/boss_settings")),
         specials = DirectorUtils.add_to_settings(name, SpecialsSettings, mod:dofile("scripts/mods/Dense Onslaught/directors/"..name.."/special_settings")),
         roaming = DirectorUtils.add_to_settings(name, RoamingSettings, mod:dofile("scripts/mods/Dense Onslaught/directors/"..name.."/roaming_settings")),
         pack_spawning = DirectorUtils.add_to_settings(name, PackSpawningSettings, mod:dofile("scripts/mods/Dense Onslaught/directors/"..name.."/packspawn_settings")),
         horde = DirectorUtils.add_to_settings(name, HordeSettings, mod:dofile("scripts/mods/Dense Onslaught/directors/"..name.."/horde_settings")),
+        pacing = DirectorUtils.add_to_settings(name, PacingSettings, mod:dofile("scripts/mods/Dense Onslaught/directors/"..name.."/pacing_settings")),
         factions = {}   
     }
 end
