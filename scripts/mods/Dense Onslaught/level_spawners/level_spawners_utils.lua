36b3e198ea6e4cace8bd8f9cbe8f121780b4b9cb
dalokraff <87997987+dalokraff@users.noreply.github.com>
2023-05-29 01:15:44 -0500

    add custom level spawner logic

---
+++ scripts/mods/Dense Onslaught/level_spawners/level_spawners_utils.lua
@@ -0,0 +1,35 @@
+local mod = get_mod("Dense Onslaught")
+local LevelSpawnerUtils = LevelSpawnerUtils or {}
+
+mod.custom_spawners_by_level = mod.custom_spawners_by_level or {}
+
+LevelSpawnerUtils.initalize_custom_spawner = function(name)
+
+    local custom_spawn_data = mod:dofile("scripts/mods/Dense Onslaught/level_spawners/levels/"..name)
+    mod.custom_spawners_by_level[name] = custom_spawn_data
+
+end
+
+LevelSpawnerUtils.spawn_funcs = {
+    setup_custom_raw_spawner = function (world, unit_name, terror_event_id, position, rotation, spawner_table, hidden)
+        local unit = World.spawn_unit(world, unit_name, location, rotation)
+        Unit.set_data(unit, "terror_event_id", terror_event_id)
+        Unit.set_data(unit, "extensions", 0, "AISpawner")
+        spawner_table[#spawner_table + 1] = unit
+        return unit
+    end,
+    setup_custom_horde_spawner = function (world, unit_name, terror_event_id, position, rotation, spawner_table, hidden)
+        local unit = World.spawn_unit(world, unit_name, position, rotation)
+        Unit.set_data(unit, "terror_event_id", terror_event_id)
+        Unit.set_data(unit, "hidden", hidden)
+        Unit.set_data(unit, "spawner_settings", "spawner1", "enabled", true)
+        Unit.set_data(unit, "spawner_settings", "spawner1", "node", "a_spawner_start")
+        Unit.set_data(unit, "spawner_settings", "spawner1", "spawn_rate", 2)
+        Unit.set_data(unit, "spawner_settings", "spawner1", "animation_events", 0, "spawn_idle")
+        Unit.set_data(unit, "extensions", 0, "AISpawner")
+        spawner_table[#spawner_table + 1] = unit
+        return unit
+    end,
+}
+
+return DirectorUtils
---
+++ scripts/mods/Dense Onslaught/level_spawners/levels/skittergate.lua
@@ -0,0 +1,48 @@
+local custom_spawn_data = {
+    
+    {
+        position = Vector3Box(-271.67, -355.88, -122.12),
+        terror_event_id = "onslaught_gate_guard",
+        spawn_func = "setup_custom_raw_spawner",
+        hidden = true,
+        rotation = QuaternionBox(Quaternion.from_elements(0, 0, -0.112, -0.994)),
+    },
+    {
+        position = Vector3Box(265.35, 481.66, -16.1),
+        terror_event_id = "onslaught_CW_gatekeeper_1",
+        spawn_func = "setup_custom_horde_spawner",
+        hidden = true,
+    },
+    {
+        position = Vector3Box(259.66, 442.29, -14.23),
+        terror_event_id = "onslaught_CW_gatekeeper_2",
+        spawn_func = "setup_custom_horde_spawner",
+        hidden = true,
+    },
+    {
+        position = Vector3Box(281.45, 474, -14.85),
+        terror_event_id = "onslaught_CW_gatekeeper_3",
+        spawn_func = "setup_custom_horde_spawner",
+        hidden = true,
+    },
+    {
+        position = Vector3Box(269.59, 432.6, -8.99),
+        terror_event_id = "onslaught_zerker_gatekeeper",
+        spawn_func = "setup_custom_horde_spawner",
+        hidden = true,
+    },
+    {
+        position = Vector3Box(237, 438.64, -6.85),
+        terror_event_id = "onslaught_zerker_gatekeeper",
+        spawn_func = "setup_custom_horde_spawner",
+        hidden = true,
+    },
+    {
+        position = Vector3Box(281.45, 474, -14.85),
+        terror_event_id = "onslaught_zerker_gatekeeper",
+        spawn_func = "setup_custom_horde_spawner",
+        hidden = true,
+    },
+}
+
+return custom_spawn_data
