2a3a753e05c7e7bb4e78b99be058d147c7baf282
John Hiles <hilesj@vcu.edu>
2023-06-15 16:32:07 -0400

    Major file restructure. Mod not functional in current state.

--- scripts/mods/Dense Onslaught/mutator/terror_event_settings/hooks.lua
+++ scripts/mods/Dense Onslaught/mutator/terror_event_settings/hooks.lua
@@ -21,3 +21,17 @@ mod:hook(TerrorEventMixer, "add_to_start_event_list", function (func, event_name
     
     return func(event_name, seed, origin_unit, origin_position)
 end)
+
+--seems to be needed to fix a bug with base game interaction between _execute_event_horde and roll_easy with HordeCompositions 
+mod:hook(LoadedDice, "roll_easy", function(func, loaded_table)
+	local new_table = loaded_table
+	if type(loaded_table[2]) ~= "table" then
+		new_table = {
+			loaded_table[1],
+			{
+				1,
+			}
+		}
+	end
+	return func(new_table) or 1
+end)
